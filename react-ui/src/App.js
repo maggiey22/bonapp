6750868480d862be32666513ad1ef76fc6ca5f62
MagY <yangm110615@gmail.com>
2020-05-06 14:03:37 -0700

    routing

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,69 +1,55 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
+/*
+Cooking Tree https://www.youtube.com/channel/UCtby6rJtBGgUm-2oD_E7bzw
+HidaMari https://www.youtube.com/channel/UCcp9uRaBwInxl_SZqGRksDA
+
+小麦粉だいすき https://www.youtube.com/user/taira1588/videos
+
+seonkyoung longest
+https://www.youtube.com/watch?v=ITp-8U5hCbQ
+
+joc https://www.youtube.com/channel/UCvBtKQaoDhsHkrvtLjSAhyw
+emma's goodies https://www.youtube.com/channel/UCgmOd6sRQRK7QoSazOfaIjQ
+joshua weissman https://www.youtube.com/channel/UChBEbMKI1eCcejTtmI32UEw
+sweet the mi https://www.youtube.com/channel/UCW3Zwj1ntR4I8OxEJvTM5XA
+honeykki https://www.youtube.com/channel/UCvQPUPoMK0Smj-OHeUe9SEw
+cooking with dog https://www.youtube.com/channel/UCpprBWvibvmOlI8yJOEAAjA
+
+adam rag https://www.youtube.com/channel/UC9_p50tH3WmMslWRWKnM7dQ
+
+*/
+
+import Navbar from './components/navbar.component';
+import IngredientList from './components/search.component';
+import Results from './components/results.component';
+
+const Page_404 = props => (
+  <div>
+    <h3>404 Page Not Found!</h3>
+    <Link to="/">Search</Link>
+  <script>
+    {window.location = "/"}
+  </script>
+  </div>
+);
+
 function App() {
-  const [message, setMessage] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
-  const [url, setUrl] = useState('/api');
-
-  const fetchData = useCallback(() => {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setMessage(json.message);
-        setIsFetching(false);
-      }).catch(e => {
-        setMessage(`API call failed: ${e}`);
-        setIsFetching(false);
-      })
-  }, [url]);
-
-  useEffect(() => {
-    setIsFetching(true);
-    fetchData();
-  }, [fetchData]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        { process.env.NODE_ENV === 'production' ?
-            <p>
-              This is a production build from create-react-app.
-            </p>
-          : <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-        }
-        <p>{'« '}<strong>
-          {isFetching
-            ? 'Fetching message from API'
-            : message}
-        </strong>{' »'}</p>
-        <p><a
-          className="App-link"
-          href="https://github.com/mars/heroku-cra-node"
-        >
-          React + Node deployment on Heroku
-        </a></p>
-        <p><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a></p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <br/>
+        <Switch>
+          <Route path="/" exact component={IngredientList} />
+          <Route path="/results" exact component={Results} />
+          <Route component={Page_404}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
-
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,42 +1,44 @@
 const express = require('express');
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const cors = require('cors');
 
-const isDev = process.env.NODE_ENV !== 'production';
+// const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
+const app = express();
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+app.use(cors());
+app.use(express.json()); // parse json
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
+// Answer API requests.
+/* app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+}); */
 
-} else {
-  const app = express();
+// one mistake -- i was using get
+// WORKING!
+/* app.post('/search', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(`{"message":"Hello world!", "items": ${req.body.items}}`);
+}); */
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+// working!
+// app.post('/search', (req, res) => {
+//   console.log("SENDING ITEMS!!!");
+//   res.json(req.body.items);
+// });
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
+const searchRouter = require('./search');
+app.use('/search', searchRouter);
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
 
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
