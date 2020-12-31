d454deac31b0f6ead7be708106f0e87f2cb523b6
Maggie <43491401+maggiey22@users.noreply.github.com>
2020-12-30 22:39:14 -0800

    switch IngredientList and Settings to functional components

--- react-ui/src/components/Settings.js
+++ react-ui/src/components/Settings.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ls from 'local-storage';
 
-export default class Settings extends Component {
-    render() {
-        return (
-            <div>
-                <p>You are on the Settings component!</p>
-                <p>counter: {ls.get('counter')}</p>
-                <p>ingredients: {JSON.stringify(ls.get('ingredients'))}</p>
-                <p>results: {JSON.stringify(ls.get('results'))}</p>
-            </div>
-        )
-    }
+function Settings() {
+    return (
+        <div>
+            <p>You are on the Settings component!</p>
+            <p>counter: {ls.get('counter')}</p>
+            <p>ingredients: {JSON.stringify(ls.get('ingredients'))}</p>
+            <p>results: {JSON.stringify(ls.get('results'))}</p>
+        </div>
+    )
 }
+
+export default Settings;
