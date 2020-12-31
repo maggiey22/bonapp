12ae460dbc2828db9e77093b132c7de5d8eecfea
Maggie <43491401+maggiey22@users.noreply.github.com>
2020-12-31 00:13:01 -0800

    refactor Ingredient components + cleanup

--- react-ui/src/components/Navbar.js
+++ react-ui/src/components/Navbar.js
@@ -3,17 +3,18 @@ import { NavLink } from 'react-router-dom';
 
 import './Navbar.css';
 
-function Navbar(props) {
+function Navbar() {
     // console.log(this.props.activeTab); TODO - why does it print twice consecutively?
+
+    // removed onClick={(e) => { e.preventDefault(); props.search();}} from Recipes NavLink to reduce API requests (otherwise it searches again on click) 
     return (
         <nav className="navbar">
             <span><NavLink className="tab" activeClassName="active-tab" exact to="/">Search</NavLink></span>
-            <span><NavLink className="tab" activeClassName="active-tab" to="/recipes" onClick={(e) => { e.preventDefault(); props.search();}}>Recipes</NavLink></span>
+            <span><NavLink className="tab" activeClassName="active-tab" to="/recipes">Recipes</NavLink></span>
             <span><NavLink className="tab" activeClassName="active-tab" to="/settings">Settings</NavLink></span>
             <span><NavLink className="tab" activeClassName="active-tab" to="/about">About</NavLink></span>
         </nav>
     );
-
 }
 
 export default Navbar;
--- server/index.js
+++ server/index.js
@@ -7,36 +7,30 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // parse json
 
-// Priority serve any static files.
-// app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-// Answer API requests.
-/* app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-}); */
-
-// one mistake -- i was using get
-// WORKING!
-/* app.post('/search', (req, res) => {
-  res.set('Content-Type', 'application/json');
-  res.send(`{"message":"Hello world!", "items": ${req.body.items}}`);
-}); */
-
-// working!
-// app.post('/search', (req, res) => {
-//   console.log("SENDING ITEMS!!!");
-//   res.json(req.body.items);
-// });
-
 const searchRouter = require('./search');
 app.use('/search', searchRouter);
-
-// All remaining requests return the React app, so it can handle routing.
-// app.get('*', function(request, response) {
-//   response.sendFile(path.resolve(__dirname, '../react-ui/public', 'index.html'));
-// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+/*
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+// one mistake -- i was using get
+// WORKING!
+app.post('/search', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(`{"message":"Hello world!", "items": ${req.body.items}}`);
+});
+
+// working!
+app.post('/search', (req, res) => {
+  console.log("SENDING ITEMS!!!");
+  res.json(req.body.items);
+});
+*/
