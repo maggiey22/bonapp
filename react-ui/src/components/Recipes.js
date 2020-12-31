5f365fd32c2424dcef3a650edc485fb947ef42c1
Maggie <43491401+maggiey22@users.noreply.github.com>
2020-12-30 22:27:10 -0800

    style recipes tab

--- react-ui/src/components/Recipes.js
+++ react-ui/src/components/Recipes.js
@@ -16,7 +16,7 @@ function Recipes(props) {
                 {results.length} results found.
                 {results.map((vid) =>
                     <div className="video" key={vid.id}>
-                        <button className="button" onClick={() => setText(`${vid.desc}`)}>?</button>
+                        <button className="info-btn" onClick={() => setText(`${vid.desc}`)}>?</button>
                         <iframe title="Video title here" width="276" height="156" frameBorder="0" src={`https://www.youtube.com/embed/${vid.id}`} allow="encrypted-media" allowFullScreen></iframe>
                     </div>)}
             </div>
