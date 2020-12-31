b52e2b10c9e92d3d4345d518107c30387bdf4dcf
Maggie <43491401+maggiey22@users.noreply.github.com>
2020-12-30 22:39:14 -0800

    switch IngredientList and Settings to functional components

--- react-ui/src/components/IngredientList.js
+++ react-ui/src/components/IngredientList.js
@@ -1,132 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Ingredient from './Ingredient';
 
-// const isDev = process.env.NODE_ENV !== 'production';
-
-export default class IngredientList extends Component {
-    /*
-    constructor(props) {
-        super(props);
-
-        this.state = {ingredients: []};
-    }
-    */
-
-    deleteAllIngredients = () => {
-        
-    }
-    // i think i need a child component here
-
-    render() {
-        // return this.props.ingredients.map((ingredient) => (
-        //     <Ingredient id={ingredient.id} key={ingredient.id} ingredient={ingredient} deleteIngredient={this.props.deleteIngredient} />
-        // ));
-
-        return (
+function IngredientList(props) {
+    return (
         <div>
-        {this.props.ingredients.map((ingredient) =>
-            <Ingredient id={ingredient.id} key={ingredient.id} ingredient={ingredient} deleteIngredient={this.props.deleteIngredient} />
+        {props.ingredients.map((ingredient) =>
+            <Ingredient id={ingredient.id} key={ingredient.id} ingredient={ingredient} deleteIngredient={props.deleteIngredient} />
         )}
         </div>
-        );
-    }
-
+    );
 }
 
-/*
-render() {
-        return (
-            <input type="text"
-                required
-                className="form-control"
-                value={this.state.name}
-                onChange={this.onChangeItem}
-                onKeyPress={this.onKeyPressed}
-            />
-        );
-    }
-
-
-
-export default class IngredientList extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.onChangeItems = this.onChangeItems.bind(this);
-        this.onSubmit = this.onSubmit.bind(this); // got a this.state is undefined without this line
-        this.onKeyPressed = this.onKeyPressed.bind(this);
-
-        this.state = {
-            items: []
-            // items: ''
-        }
-    }
-
-    onChangeItems(e) {
-        // this is really bad because it's splitting anytime the text in the box changes
-        let arr = e.target.value.split(',');
-        
-        // console.log(arr);
-
-        this.setState({
-            items: arr
-            // items: e.target.value
-        });
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-        
-        let arr = this.state.items;
-        arr.forEach((item, i) => {arr[i] = item.trim()});
-        // trim returns a string lmao :|
-
-        const list = {
-            items: arr
-        }
-
-        // console.log(list);
-        console.log("Searching...");
-
-        // axios.post('http://localhost:5000/search', list)
-        
-        axios.post(isDev? 'http://localhost:5000/search'
-                        : 'https://youtube-recipe-finder.herokuapp.com/search',
-                        list)
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err));
-
-        window.location = '/results';
-    }
-
-    onKeyPressed(e) {
-        if (e.key === 'Enter') {
-            console.log('Key pressed!');
-        }
-    }
-
-    render() {
-        // onKeyPress={this.onKeyPressed}
-        return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <label>Search items: </label>
-                    <input type="text"
-                        required
-                        className="form-control"
-                        value={this.state.items}
-                        onChange={this.onChangeItems}
-                    />
-
-                    <div id="n-list">
-
-                    </div>
-
-                    <input type="submit" value="Search" />
-                </form>
-            </div>
-        )
-    }
-}
-*/
+export default IngredientList;
